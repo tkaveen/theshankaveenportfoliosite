753ae735f27c5d1e216a560bb097b87e8b2b0b73
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-07-22 13:43:56 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
-  return <div className="App">theshan kaveen</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+// import Toggle from "../ToggleButton/Toggle";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Theshan Kaveen</div>
+        {/* <Toggle /> */}
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Portfolio</li>
+            <li>Recommendations</li>
+          </ul>
+        </div>
+        <button className="button n-button">Contact</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
